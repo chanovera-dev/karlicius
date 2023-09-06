cfce14d973eefd18e730f68b13309f5590755c12
Chano Vera <contacto@chanovera.com>
2023-09-06 16:21:51 -0600

    tienda - limpieza

--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -38,16 +38,16 @@ require_once(get_template_directory() . '/functions/woocommerce/wc-components.ph
 //Disable all woocommerce stylesheets
 add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
 
-function disable_woocommerce_block_styles() {
-	wp_deregister_style( 'wc-blocks-style' );
-  }
-  add_action( 'wp_enqueue_scripts', 'disable_woocommerce_block_styles' );
-
-  /**
- * Disable WooCommerce block styles (back-end).
- */
-function disable_woocommerce_block_editor_styles() {
-	wp_deregister_style( 'wc-block-editor' );
-	wp_deregister_style( 'wc-blocks-style' );
-  }
-  add_action( 'enqueue_block_assets', 'disable_woocommerce_block_editor_styles', 1, 1 );
+function disable_wp_blocks() {
+	$wstyles = array("wc-blocks-style","wc-blocks-style-active-filters","wc-blocks-style-add-to-cart-form","wc-blocks-packages-style","wc-blocks-style-all-products","wc-blocks-style-all-reviews","wc-blocks-style-attribute-filter","wc-blocks-style-breadcrumbs","wc-blocks-style-catalog-sorting","wc-blocks-style-customer-account","wc-blocks-style-featured-category","wc-blocks-style-featured-product","wc-blocks-style-mini-cart","wc-blocks-style-price-filter","wc-blocks-style-product-add-to-cart","wc-blocks-style-product-button","wc-blocks-style-product-categories","wc-blocks-style-product-image","wc-blocks-style-product-image-gallery","wc-blocks-style-product-query","wc-blocks-style-product-results-count","wc-blocks-style-product-reviews","wc-blocks-style-product-sale-badge","wc-blocks-style-product-search","wc-blocks-style-product-sku","wc-blocks-style-product-stock-indicator","wc-blocks-style-product-summary","wc-blocks-style-product-title","wc-blocks-style-rating-filter","wc-blocks-style-reviews-by-category","wc-blocks-style-reviews-by-product","wc-blocks-style-product-details","wc-blocks-style-single-product","wc-blocks-style-stock-filter","wc-blocks-style-cart","wc-blocks-style-checkout","wc-blocks-style-mini-cart-contents","classic-theme-styles-inline");
+	
+	foreach ($wstyles as $wstyle){
+	 wp_deregister_style($wstyle);  
+	}
+	
+	$wscripts = array("wc-blocks-middleware","wc-blocks-data-store");
+	 foreach ($wscripts as $wscript){
+	 wp_deregister_script($wscript);  
+	}
+}
+add_action( "init", "disable_wp_blocks",100 );
