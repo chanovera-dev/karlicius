400f5bfc913157e99bd490622780633f68b2ab95
Chano Vera <contacto@chanovera.com>
2023-09-06 12:46:25 -0600

    carrito tiempo real

--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -1,1 +1,15 @@
 <?php
+
+//Show cart contents / total Ajax
+add_filter( 'woocommerce_add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment' );
+function woocommerce_header_add_to_cart_fragment( $fragments ) {
+	global $woocommerce;
+	ob_start();
+	?>
+	<a class="cart-customlocation" href="<?php echo esc_url(wc_get_cart_url()); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>">
+	    <?php echo sprintf(_n('%d', '%d', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count);?>
+	</a>
+	<?php
+	$fragments['a.cart-customlocation'] = ob_get_clean();
+	return $fragments;
+}
