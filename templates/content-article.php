64a20badea30913a80ae0fd376c5e9f6954cc189
Chano Vera <contacto@chanovera.com>
2023-09-06 12:15:51 -0600

    post

---
+++ templates/content-article.php
@@ -0,0 +1,27 @@
+<?php
+echo '<main id="main" class="main-content">
+    <article>';
+        include(TEMPLATEPATH . '/parts/sections/title-article.php');
+        echo '<section class="container">
+            <div class="section content-section padding-section">
+                <div class="content">';
+                    the_content();
+                echo '</div>';
+                include(TEMPLATEPATH . '/parts/sidebars/post.php');
+            echo '</div>
+        </section>
+        <section class="container comments-section-wrapper">
+            <div class="section comments-section padding-section">
+                <div class="comments">';
+                    comments_template();
+                echo '</div>';    
+                if ( comments_open() ) : 
+                    echo '<aside>';
+                        include(TEMPLATEPATH . '/parts/widgets/address.php');
+                    echo '</aside>';
+                else:
+                endif;
+            echo '</div>
+        </section>
+    </article>
+</main>';
