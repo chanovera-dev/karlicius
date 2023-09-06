f9756c83d8c1fbbbc62fb52d6c4c2e83ee471728
Chano Vera <contacto@chanovera.com>
2023-09-06 10:36:31 -0600

    estado inicial básico

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+echo '<!DOCTYPE html>
+<html '; language_attributes(); echo '>
+    <head>
+        <meta charset="'; bloginfo( 'charset' ); echo '">
+        <meta name="description" content="'; bloginfo( 'description' ); echo '">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">';
+        wp_head();
+    echo '</head>
+    <body '; body_class(); echo '>
+        <header class="container main-header">
+            <section class="section header-content">';
+                if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+                    echo '<p>WooCommerce activo</p>';
+                } else {
+
+                }
+            echo '</section>
+        </header>';
