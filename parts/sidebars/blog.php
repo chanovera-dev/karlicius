29871b25c08e6a53e40e480a41b76e83babf46b6
Chano Vera <contacto@chanovera.com>
2023-09-06 11:55:05 -0600

    home

---
+++ parts/sidebars/blog.php
@@ -0,0 +1,7 @@
+<aside>
+    <?php
+        include (TEMPLATEPATH. '/parts/widgets/tags-cloud.php');
+        include (TEMPLATEPATH. '/parts/widgets/categories.php');
+        include (TEMPLATEPATH. '/parts/widgets/archive-list.php');
+    ?>
+</aside>
