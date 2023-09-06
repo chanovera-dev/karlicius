4dccd3d6fe7a4ce13538557114e7c3db3d35feb7
Chano Vera <contacto@chanovera.com>
2023-09-06 15:19:02 -0600

    tienda - limpieza

--- functions/woocommerce/wc-components.php
+++ functions/woocommerce/wc-components.php
@@ -4,7 +4,7 @@
 function shop_styles() {
     if ( is_shop() ) {
         wp_dequeue_style( 'wp-block-library' );
-        wp_dequeue_style( 'wc-blocks' );
+        wp_dequeue_style( 'active-filters' );
         wp_enqueue_style( 'shop-styles', get_template_directory_uri() . '/assets/css/shop.css' );
     }
 }
