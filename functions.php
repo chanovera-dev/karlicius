b77892f66a78f98ec9df7381073ac253a8275fdd
Chano Vera <contacto@chanovera.com>
2023-09-06 11:21:12 -0600

    se agregan los estilos específicos y 'functions/components.php'

--- functions.php
+++ functions.php
@@ -29,6 +29,9 @@ register_nav_menus(
 // Anexo para establecer las variables de los colores
 require_once(get_template_directory() . '/functions/colors.php');
 
+// Anexo para establecer los componentes de los templates
+require_once(get_template_directory() . '/functions/components.php');
+
 // activa woocommerce
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
     require_once(get_template_directory() . '/functions/woocommerce.php');
