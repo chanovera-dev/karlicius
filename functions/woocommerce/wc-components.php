9242cf7002bc957531216f544cf979b1343cea98
Chano Vera <contacto@chanovera.com>
2023-09-06 15:09:10 -0600

    tienda - botón de agregar a carrito

--- functions/woocommerce/wc-components.php
+++ functions/woocommerce/wc-components.php
@@ -4,6 +4,7 @@
 function shop_styles() {
     if ( is_shop() ) {
         wp_dequeue_style( 'wp-block-library' );
+        wp_dequeue_style( 'wc-blocks' );
         wp_enqueue_style( 'shop-styles', get_template_directory_uri() . '/assets/css/shop.css' );
     }
 }
