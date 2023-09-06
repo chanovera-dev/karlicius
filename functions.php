d3d0fdfcf8273113163e9f77a1303ecbca592ad0
Chano Vera <contacto@chanovera.com>
2023-09-06 11:08:32 -0600

    agregados los colores

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 // carga componentes (estilos, javascript, etc) en el header
 function load_parts_header() {
     // Estilos globales
-    wp_register_style( 'global', get_template_directory_uri() . '/style.css', '', 1, 'all' ); 
+    wp_register_style( 'global', get_template_directory_uri() . '/style.css', '', 1, 'all' );
     wp_enqueue_style( 'global' );
 }
 add_action( 'wp_enqueue_scripts', 'load_parts_header' );
@@ -11,7 +11,9 @@ add_action( 'wp_enqueue_scripts', 'load_parts_header' );
 // Carga componentes (estilos, javascript, etc) en el footer
 function load_parts_footer(){
     // JS de efectos en la cabecera
-    wp_enqueue_script( 'header', get_template_directory_uri() . '/assets/js/header.js', '', 1, true );   
+    wp_enqueue_script( 'header', get_template_directory_uri() . '/assets/js/header.js', '', 1, true ); 
+    /* estilos css para los formularios */
+    wp_enqueue_style( 'forms-styles', get_template_directory_uri() . '/assets/css/forms.css' ); 
 }
 add_action( 'get_footer', 'load_parts_footer' );
 
