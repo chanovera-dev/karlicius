ca78f391cfa10b6bad6b64cead0bb2bc937c34b1
Chano Vera <contacto@chanovera.com>
2023-09-07 13:57:57 -0600

    reviews product

--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -52,32 +52,8 @@ function disable_wp_blocks() {
 }
 add_action( "init", "disable_wp_blocks",100 );
 
-add_filter( 'comment_form_defaults', 'render_pros_cons_fields_for_anonymous_users', 10, 1 );
-add_action( 'comment_form_top', 'render_pros_cons_fields_for_authorized_users' );
-function get_pros_cons_fields_html() {
-	ob_start();
-	?>
+add_action( 'woocommerce_comment_pagination_args', 'address_on_reviews', 30 );
 
-	<p>Prueba</p>
-
-	<?php
-	return ob_get_clean();
-}
-
-function render_pros_cons_fields_for_authorized_users() {
-	if ( ! is_product() || ! is_user_logged_in() ) {
-		return;
-	}
-	
-	echo get_pros_cons_fields_html();
-}
-
-function render_pros_cons_fields_for_anonymous_users( $defaults ) {
-	if ( ! is_product() || is_user_logged_in() ) {
-		return;
-	}
-	
-	$defaults['comment_notes_before'] .= get_pros_cons_fields_html();
-	
-	return $defaults;
+function address_on_reviews() {
+	echo 'prueba';
 }
