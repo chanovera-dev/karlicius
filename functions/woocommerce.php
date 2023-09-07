66f24ce25aae4a8833c8b539132cd0ac5bbafa0c
Chano Vera <contacto@chanovera.com>
2023-09-06 20:30:20 -0600

    tienda - icono a agregar al carrito

--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -54,8 +54,8 @@ add_action( "init", "disable_wp_blocks",100 );
 
 add_filter('woocommerce_loop_add_to_cart_link', 'woo_add_to_cart_replace', 10, 2 ); // shop
 function woo_add_to_cart_replace() {
-    global $product;
-    $link = $product->get_permalink();
+    
+    $link = esc_url(get_permalink());
     echo do_shortcode('<a href="'.$link.'" class="button add_to_cart_button ajax_add_to_cart"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"/><path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/></svg>Agregar al carrito</a>');
 }
 
