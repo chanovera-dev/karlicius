7a5260b3600f6863440bcdeb3242e5f94356cbe5
Chano Vera <contacto@chanovera.com>
2023-09-06 18:24:43 -0600

    tienda - icono a agregar al carrito

--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -57,7 +57,7 @@ add_filter('woocommerce_product_add_to_cart_text', 'bryce_archive_add_to_cart_te
 
 add_filter('woocommerce_product_add_to_cart_text', 'bryce_archive_add_to_cart_text');
 
-function bryce_archive_add_to_cart_text($text)
+function bryce_archive_add_to_cart_text($link)
 {
     $icon_svg = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
         <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
