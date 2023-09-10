721f82c9831de35b4e5ceafe121f7dea1da3bc84
Chano Vera <contacto@chanovera.com>
2023-09-10 00:09:41 -0600

    reviews product y mensajes

--- functions/woocommerce/wc-icons.php
+++ functions/woocommerce/wc-icons.php
@@ -10,21 +10,17 @@ function theme_icons() {
             .woocommerce p.stars a.star-3:hover:before,
             .woocommerce p.stars a.star-4:hover:before,
             .woocommerce p.stars a.star-5:hover:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/star-fill.svg');}
-            .woocommerce p.stars a.star-1.active:before,
-            .woocommerce p.stars a.star-2.active:before,
-            .woocommerce p.stars a.star-3.active:before,
-            .woocommerce p.stars a.star-4.active:before,
-            .woocommerce p.stars a.star-5.active:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/star-fill.svg');}
+            
 
             .woocommerce p.stars a.star-1.active:before,
             .woocommerce p.stars a.star-2.active:before,
             .woocommerce p.stars a.star-3.active:before,
             .woocommerce p.stars a.star-4.active:before,
             .woocommerce p.stars a.star-5.active:before,
-            .woocommerce p.stars a.star-2:hover ~ a:before,
-            .woocommerce p.stars a.star-3:hover ~ a:before,
+            .woocommerce p.stars a.star-5:hover ~ a:before,
             .woocommerce p.stars a.star-4:hover ~ a:before,
-            .woocommerce p.stars a.star-5:hover ~ a:before {
+            .woocommerce p.stars a.star-3:hover ~ a:before,
+            .woocommerce p.stars a.star-2:hover ~ a:before {
                 content: '';
                 background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/star-fill.svg');
             }
