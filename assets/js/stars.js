c641f17664d53011b82c6ffdf504d5a5a3ec6dfd
Chano Vera <contacto@chanovera.com>
2023-09-10 01:17:39 -0600

    reviews product y mensajes

--- assets/js/stars.js
+++ assets/js/stars.js
@@ -1,5 +1,5 @@
-let star1 = document.querySelectorAll('.star-1');
-let star2 = document.querySelectorAll('.star-2');
+let star1 = document.getElementsByClassName('.star-1');
+let star2 = document.getElementsByClassName('.star-2');
 
 
 star2.addEventListener('mouseover', () => {
