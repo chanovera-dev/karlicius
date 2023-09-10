6d3264e19aca4a38c50d4500f566d225855165d1
Chano Vera <contacto@chanovera.com>
2023-09-10 01:19:03 -0600

    reviews product y mensajes

--- assets/js/stars.js
+++ assets/js/stars.js
@@ -1,11 +1,20 @@
-let star1 = document.getElementsByClassName('.star-1');
-let star2 = document.getElementsByClassName('.star-2');
+// Obtén todos los elementos con la clase 'star-1' y 'star-2'
+let star1 = document.getElementsByClassName('star-1');
+let star2 = document.getElementsByClassName('star-2');
 
+// Agrega eventos a 'star2' (teniendo en cuenta que es una colección)
+for (let i = 0; i < star2.length; i++) {
+    star2[i].addEventListener('mouseover', () => {
+        // Itera sobre los elementos de 'star1' y agrega la clase 'filled' a cada uno
+        for (let j = 0; j < star1.length; j++) {
+            star1[j].classList.add('filled');
+        }
+    });
 
-star2.addEventListener('mouseover', () => {
-    star1.classList.add('filled');
-});
-
-star2.addEventListener('mouseout', () => {
-    star1.classList.remove('filled');
-});
+    star2[i].addEventListener('mouseout', () => {
+        // Itera sobre los elementos de 'star1' y quita la clase 'filled' de cada uno
+        for (let j = 0; j < star1.length; j++) {
+            star1[j].classList.remove('filled');
+        }
+    });
+}
