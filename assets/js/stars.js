e92cc32408064e458514279dc061ad5d29c8b4c8
Chano Vera <contacto@chanovera.com>
2023-09-09 23:47:31 -0600

    reviews product y mensajes

--- assets/js/stars.js
+++ assets/js/stars.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', () => {
     const stars = document.querySelectorAll(".woocommerce p.stars a");
 
     stars.forEach(function(star, index) {
