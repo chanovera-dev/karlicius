60d38054ddd59d3c4bd2366eb5a83f4b15cd039d
Chano Vera <contacto@chanovera.com>
2023-09-10 00:30:25 -0600

    reviews product y mensajes

--- functions/woocommerce/wc-icons.php
+++ functions/woocommerce/wc-icons.php
@@ -16,10 +16,10 @@ function theme_icons() {
             .woocommerce p.stars a.star-4:hover:before,
             .woocommerce p.stars a.star-5:hover:before {
                 content: '';
-                background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/star.svg');
+                background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/star-fill.svg');
             }
 
-            /* Al hacer clic en una estrella o agregar la clase active, todas las estrellas anteriores se llenan */
+            /* Al hacer clic en una estrella o agregar la clase active, todas las estrellas anteriores se llenan *
             .woocommerce p.stars a.star-1.active:before,
             .woocommerce p.stars a.star-2.active:before,
             .woocommerce p.stars a.star-3.active:before,
@@ -28,7 +28,7 @@ function theme_icons() {
             .woocommerce p.stars a.star-2:hover ~ a:before,
             .woocommerce p.stars a.star-3:hover ~ a:before,
             .woocommerce p.stars a.star-4:hover ~ a:before,
-            .woocommerce p.stars a.star-5:hover ~ a:before {content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/star-fill.svg');}
+            .woocommerce p.stars a.star-5:hover ~ a:before {content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/star-fill.svg');}*/
         </style>
     <?php
 }
