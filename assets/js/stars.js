fa75df1fc0d801899aac37106ec19f9f7a97258e
Chano Vera <contacto@chanovera.com>
2023-09-10 10:53:21 -0600

    reviews product y mensajes

---
+++ assets/js/stars.js
@@ -0,0 +1,13 @@
+const stars = document.querySelectorAll('.stars span a');
+
+stars.forEach((star, index) => {
+  star.addEventListener('mouseenter', () => {
+    for (let i = 0; i <= index; i++) {
+      stars[i].classList.add('active');
+    }
+  });
+
+  star.addEventListener('mouseleave', () => {
+    stars.forEach((s) => s.classList.remove('active'));
+  });
+});
