b2294806e14bf2090b320981cd187374c6dd4306
Chano Vera <contacto@chanovera.com>
2023-09-09 23:30:25 -0600

    reviews product y mensajes

--- assets/js/stars.js
+++ assets/js/stars.js
@@ -3,19 +3,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
     stars.forEach(function(star, index) {
         star.addEventListener("mouseenter", function() {
-            // Agregar una clase 'hover' a la estrella actual
-            star.classList.add("hover");
+            // Agregar una clase 'filled' a la estrella actual
+            star.classList.add("filled");
 
-            // Cambiar el icono de fondo de las estrellas anteriores
+            // Agregar una clase 'filled' a las estrellas anteriores
             for (let i = 0; i < index; i++) {
                 stars[i].classList.add("filled");
             }
         });
 
         star.addEventListener("mouseleave", function() {
-            // Quitar la clase 'hover' de la estrella actual
-            star.classList.remove("hover");
-
             // Quitar la clase 'filled' de todas las estrellas
             stars.forEach(function(star) {
                 star.classList.remove("filled");
