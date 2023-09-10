abf1223082d4ce4c9b33f5f432ae8348acd08a17
Chano Vera <contacto@chanovera.com>
2023-09-10 00:15:24 -0600

    reviews product y mensajes

--- functions/woocommerce/wc-icons.php
+++ functions/woocommerce/wc-icons.php
@@ -9,37 +9,27 @@ function theme_icons() {
     background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/star.svg');
 }
 
-/* Al pasar el ratón sobre una estrella o hacer clic, las estrellas anteriores se llenan progresivamente */
-.woocommerce p.stars a.star-1.active:before,
-.woocommerce p.stars a.star-2.active:before,
-.woocommerce p.stars a.star-3.active:before,
-.woocommerce p.stars a.star-4.active:before,
-.woocommerce p.stars a.star-5.active:before {
+/* Al pasar el ratón sobre una estrella, todas las estrellas anteriores se llenan */
+.woocommerce p.stars a.star-1:hover:before,
+.woocommerce p.stars a.star-2:hover:before,
+.woocommerce p.stars a.star-3:hover:before,
+.woocommerce p.stars a.star-4:hover:before,
+.woocommerce p.stars a.star-5:hover:before {
     content: '';
     background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/star-fill.svg');
 }
 
-.woocommerce p.stars a.star-1.active:before {
-    background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/star-fill.svg');
-}
-
+/* Al hacer clic en una estrella o agregar la clase active, todas las estrellas anteriores se llenan */
+.woocommerce p.stars a.star-1.active:before,
 .woocommerce p.stars a.star-2.active:before,
-.woocommerce p.stars a.star-2:hover ~ a:before {
-    background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/star-fill.svg');
-}
-
 .woocommerce p.stars a.star-3.active:before,
-.woocommerce p.stars a.star-3:hover ~ a:before {
-    background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/star-fill.svg');
-}
-
 .woocommerce p.stars a.star-4.active:before,
-.woocommerce p.stars a.star-4:hover ~ a:before {
-    background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/star-fill.svg');
-}
-
 .woocommerce p.stars a.star-5.active:before,
+.woocommerce p.stars a.star-2:hover ~ a:before,
+.woocommerce p.stars a.star-3:hover ~ a:before,
+.woocommerce p.stars a.star-4:hover ~ a:before,
 .woocommerce p.stars a.star-5:hover ~ a:before {
+    content: '';
     background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/star-fill.svg');
 }
 
