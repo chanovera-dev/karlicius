ffd97f037795c21cb762f2d52bb69589fd0aa686
Chano Vera <contacto@chanovera.com>
2023-09-10 00:26:56 -0600

    reviews product y mensajes

--- functions/woocommerce/wc-icons.php
+++ functions/woocommerce/wc-icons.php
@@ -16,7 +16,7 @@ function theme_icons() {
             .woocommerce p.stars a.star-4:hover:before,
             .woocommerce p.stars a.star-5:hover:before {
                 content: '';
-                background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/star.svg');
+                background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/star-fill.svg');
             }
 
             /* Al hacer clic en una estrella o agregar la clase active, todas las estrellas anteriores se llenan */
@@ -28,7 +28,7 @@ function theme_icons() {
             .woocommerce p.stars a.star-2:hover ~ a:before,
             .woocommerce p.stars a.star-3:hover ~ a:before,
             .woocommerce p.stars a.star-4:hover ~ a:before,
-            .woocommerce p.stars a.star-5:hover ~ a:before {content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/star-fill.svg');}
+            .woocommerce p.stars a.star-5:hover ~ a:before {content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/star.svg');}
         </style>
     <?php
 }
