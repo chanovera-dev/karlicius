19b9b7d5156d1e5e69ed0cd7ad3d61634a3f5f4b
Chano Vera <contacto@chanovera.com>
2023-09-14 13:12:37 -0600

    add variation swatches

--- assets/js/stars.js
+++ assets/js/stars.js
@@ -1,29 +1,32 @@
 var image_to_show = '';
-var variations = JSON.parse($(".variations_form").attr("data-product_variations"));
+var variationsForm = document.querySelector(".variations_form");
+var variationsData = variationsForm.getAttribute("data-product_variations");
 
-if (variations) {
+if (variationsData) {
+    var variations = JSON.parse(variationsData);
     var first_attr = Object.keys(variations[0].attributes)[0];
+    var ulElements = document.querySelectorAll("ul[data-attribute_name='" + first_attr + "'] li");
 
-    // Cuando se hace clic en un botón de variación
-    $("ul[data-attribute_name='" + first_attr + "'] li").click(function () {
-        var choice = $(this).attr("data-value");
+    ulElements.forEach(function (li) {
+        li.addEventListener("click", function () {
+            var choice = li.getAttribute("data-value");
 
-        // Loop a través de las variaciones JSON
-        for (const i in variations) {
-            if (variations.hasOwnProperty(i)) {
-                // Si el primer atributo tiene el mismo valor que se ha seleccionado
-                if (choice === variations[i].attributes[first_attr]) {
-                    // Cambiar la imagen destacada
-                    if (variations[i].image) {
-                        image_to_show = variations[i].image.src;
+            for (var i = 0; i < variations.length; i++) {
+                var variation = variations[i];
+
+                if (variation.attributes[first_attr] === choice) {
+                    if (variation.image) {
+                        image_to_show = variation.image.src;
                     } else {
-                        // Si no hay una imagen específica para la variación, usa la imagen principal del producto.
-                        image_to_show = $(".single-product-main-image img").attr("src");
+                        var mainImage = document.querySelector(".single-product-main-image img");
+                        image_to_show = mainImage.getAttribute("src");
                     }
-                    // Actualizar la imagen
-                    $(".attachment-shop_single").attr("src", image_to_show).removeAttr("srcset");
+
+                    var shopSingleImage = document.querySelector(".attachment-shop_single");
+                    shopSingleImage.setAttribute("src", image_to_show);
+                    shopSingleImage.removeAttribute("srcset");
                 }
             }
-        }
+        });
     });
 }
