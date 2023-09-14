9941b7a1c35a30806008cf9bdb6f30bddaf6c75f
Chano Vera <contacto@chanovera.com>
2023-09-14 13:09:15 -0600

    add variation swatches

--- assets/js/stars.js
+++ assets/js/stars.js
@@ -1,30 +1,29 @@
 var image_to_show = '';
-    var variations = JSON.parse($(".variations_form").attr("data-product_variations"));
-    if(variations) {
-        var first_attr = Object.keys(variations[0].attributes)[0];
-        // when swatch button is clicked
-        $("ul[data-attribute_name='"+first_attr+"'] li").click(function() {
-            var choice = $(this).attr("data-value");
-            var found = false;
-            // loop variations JSON
-            for(const i in variations) {
-                if(found) continue;
-                if(variations.hasOwnProperty(i)) {
-                    // if first attribute has the same value as has been selected
-                    if (choice === variations[i].attributes[Object.keys(variations[0].attributes)[0]]) {
-                        // change featured image
-                        image_to_show = variations[i].image_src;
-                        found = true;
+var variations = JSON.parse($(".variations_form").attr("data-product_variations"));
+
+if (variations) {
+    var first_attr = Object.keys(variations[0].attributes)[0];
+
+    // Cuando se hace clic en un botón de variación
+    $("ul[data-attribute_name='" + first_attr + "'] li").click(function () {
+        var choice = $(this).attr("data-value");
+
+        // Loop a través de las variaciones JSON
+        for (const i in variations) {
+            if (variations.hasOwnProperty(i)) {
+                // Si el primer atributo tiene el mismo valor que se ha seleccionado
+                if (choice === variations[i].attributes[first_attr]) {
+                    // Cambiar la imagen destacada
+                    if (variations[i].image) {
+                        image_to_show = variations[i].image.src;
+                    } else {
+                        // Si no hay una imagen específica para la variación, usa la imagen principal del producto.
+                        image_to_show = $(".single-product-main-image img").attr("src");
                     }
+                    // Actualizar la imagen
+                    $(".attachment-shop_single").attr("src", image_to_show).removeAttr("srcset");
                 }
             }
-        });
-
-        // after woo additional images has changed the image, change it again
-        jQuery(".variations_form").on("wc_additional_variation_images_frontend_image_swap_done_callback", function() {
-            if(image_to_show.length) {
-                $(".attachment-shop_single").attr("src", image_to_show).removeAttr("srcset");
-            }
-        });
-
-    }
+        }
+    });
+}
