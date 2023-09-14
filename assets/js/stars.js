2436a3c3aa391648072e7f250fd9e1b654395e57
Chano Vera <contacto@chanovera.com>
2023-09-14 13:18:26 -0600

    estrellas para calificar

--- assets/js/stars.js
+++ assets/js/stars.js
@@ -1,35 +1,15 @@
 window.onload = function() {
-  var image_to_show = '';
-var variationsForm = document.querySelector(".variations_form");
-var variationsData = variationsForm.getAttribute("data-product_variations");
+  const stars = document.querySelectorAll('.stars span a');
 
-if (variationsData) {
-    var variations = JSON.parse(variationsData);
-    var first_attr = Object.keys(variations[0].attributes)[0];
-    var ulElements = document.querySelectorAll("ul[data-attribute_name='" + first_attr + "'] li");
-
-    ulElements.forEach(function (li) {
-        li.addEventListener("click", function () {
-            var choice = li.getAttribute("data-value");
-
-            for (var i = 0; i < variations.length; i++) {
-                var variation = variations[i];
-
-                if (variation.attributes[first_attr] === choice) {
-                    if (variation.image) {
-                        image_to_show = variation.image.src;
-                    } else {
-                        var mainImage = document.querySelector(".single-product-main-image img");
-                        image_to_show = mainImage.getAttribute("src");
-                    }
-
-                    var shopSingleImage = document.querySelector(".attachment-shop_single");
-                    shopSingleImage.setAttribute("src", image_to_show);
-                    shopSingleImage.removeAttribute("srcset");
-                }
-            }
-        });
+  stars.forEach((star, index) => {
+    star.addEventListener('mouseenter', () => {
+      for (let i = 0; i <= index; i++) {
+        stars[i].classList.add('active');
+      }
     });
-}
 
+    star.addEventListener('mouseleave', () => {
+      stars.forEach((s) => s.classList.remove('active'));
+    });
+  });
 };
