8a7eed8d906c76da4fda2df1cf1c632ea90d4842
Chano Vera <contacto@chanovera.com>
2023-09-14 13:22:10 -0600

    estrellas para calificar

--- assets/js/stars.js
+++ assets/js/stars.js
@@ -13,3 +13,37 @@ window.onload = function() {
     });
   });
 };
+
+var image_to_show = '';
+var variationsForm = document.querySelector(".variations_form");
+var variationsData = variationsForm.getAttribute("data-product_variations");
+
+if (variationsData) {
+    var variations = JSON.parse(variationsData);
+    var first_attr = Object.keys(variations[0].attributes)[0];
+    var ulElements = document.querySelectorAll("ul[data-attribute_name='" + first_attr + "'] li");
+
+    ulElements.forEach(function (li) {
+        li.addEventListener("click", function () {
+            var choice = li.getAttribute("data-value");
+
+            for (var i = 0; i < variations.length; i++) {
+                var variation = variations[i];
+
+                if (variation.attributes[first_attr] === choice) {
+                    if (variation.image) {
+                        image_to_show = variation.image.src;
+                    } else {
+                        var mainImage = document.querySelector(".woocommerce-product-gallery__image img");
+                        image_to_show = mainImage.getAttribute("src");
+                    }
+
+                    var shopSingleImage = document.querySelector(".attachment-shop_single");
+                    shopSingleImage.setAttribute("src", image_to_show);
+                    shopSingleImage.removeAttribute("srcset");
+                }
+            }
+        });
+    });
+}
+
