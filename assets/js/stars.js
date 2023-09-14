27dd19739040760615194e3c9d505af59c1ba48a
Chano Vera <contacto@chanovera.com>
2023-09-14 13:19:08 -0600

    estrellas para calificar

--- assets/js/stars.js
+++ assets/js/stars.js
@@ -2,7 +2,7 @@ window.onload = function() {
   const stars = document.querySelectorAll('.stars span a');
 
   stars.forEach((star, index) => {
-    star.addEventListener('mouseenter', () => {
+    star.addEventListener('mouseover', () => {
       for (let i = 0; i <= index; i++) {
         stars[i].classList.add('active');
       }
