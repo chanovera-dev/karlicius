4209db62c8ba37829e4618ab8c65b23f78bf36aa
Chano Vera <contacto@chanovera.com>
2023-09-14 11:49:12 -0600

    add variation swatches

--- templates/content-products.php
+++ templates/content-products.php
@@ -37,19 +37,20 @@
                 // Display the variations without add to cart button and quantity
                 echo '<table class="variations cfvsw-variation-disable-logic" cellspacing="0" role="presentation"><tbody><tr><td class="value">';
                 echo '<div class="cfvsw-hidden-select"><select id="colores" class="" name="attribute_colores" data-attribute_name="attribute_colores" data-show_option_none="yes"><option value="">Elige una opción</option><option value="Gris espacial" class="attached enabled">Gris espacial</option><option value="Color oro" class="attached enabled">Color oro</option><option value="Color plata" class="attached enabled">Color plata</option></select></div>';
-                echo '<div class="cfvsw-swatches-container cfvsw-product-container" swatches-attr="attribute_colores">';
+                echo '';
                 foreach ( $product->get_variation_attributes() as $attribute_name => $attribute_values ) {
-
-                          
+                    echo '<div class="cfvsw-swatches-container cfvsw-product-container" swatches-attr="attribute_colores"><div class="cfvsw-swatches-option cfvsw-label-option" data-slug="'. esc_attr( sanitize_title( $attribute_name ) ) .'" data-title="'. esc_attr( sanitize_title( $attribute_name ) ) .'" style="min-width:24px;min-height:24px;border-radius:3px;">';
+                    echo '<div class="cfvsw-swatch-inner">' . wc_attribute_label( $attribute_name ) . ':</div> ';      
                     // Display attribute values
-                    echo '<div class="cfvsw-swatches-option cfvsw-label-option" data-slug="'. esc_attr( sanitize_title( $attribute_name ) ) .'" data-title="'. esc_attr( sanitize_title( $attribute_name ) ) .'" style="min-width:24px;min-height:24px;border-radius:3px;"><div class="cfvsw-swatch-inner">' . wc_attribute_label( $attribute_name ) . ':</div></div>';
+                    echo '<span class="attribute-values">';
                     foreach ( $attribute_values as $attribute_value ) {
                         echo '<div class="cfvsw-swatch-inner">' . esc_html( $attribute_value ) . '</div>';
                     }
                     echo '</div>';   
-
+                    echo '</div>';
+                    echo '</div>';
                 } 
-                echo '</div>';
+                
                 echo '</td></tr></tbody></table>';
             }
             do_action( 'woocommerce_after_shop_loop_item_title' ); // Contiene woocommerce_template_loop_rating, woocommerce_show_product_loop_sale_flash, woocommerce_template_loop_price | Rating, Saldos y Precios
