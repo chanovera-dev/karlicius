63dce2e48312742c7b93f63cf4d514acf9c4d804
Chano Vera <contacto@chanovera.com>
2023-09-19 09:24:55 -0600

    ajuste al contenido de los datos del post sidebar

--- parts/widgets/related-posts.php
+++ parts/widgets/related-posts.php
@@ -1,22 +1,25 @@
+<?php 
+    global $post;
+
+    // Obtener las categorías del post actual
+    $post_categories = wp_get_post_categories($post->ID);
+
+    if ($post_categories) {
+        $args = array(
+            'posts_per_page' => 6,
+            'category__in' => $post_categories, // Filtrar por categorías del post actual
+            'post__not_in' => array($post->ID), // Excluir el post actual
+        );
+
+        $related_posts = get_posts($args);
+
+        if ($related_posts) { // Verificar si hay contenidos relacionados
+?>
 <div class="latest-posts_widget">
     <div class="title-wrapper">
         <h3 class="title"><?= __('Artículos relacionados', 'karlicius') ?></h3>
     </div>
     <?php 
-        global $post;
-
-        // Obtener las categorías del post actual
-        $post_categories = wp_get_post_categories($post->ID);
-
-        if ($post_categories) {
-            $args = array(
-                'posts_per_page' => 6,
-                'category__in' => $post_categories, // Filtrar por categorías del post actual
-                'post__not_in' => array($post->ID), // Excluir el post actual
-            );
-
-            $related_posts = get_posts($args);
-
             foreach ($related_posts as $post) :
                 setup_postdata($post);
 
@@ -24,6 +27,9 @@
 
             endforeach;
             wp_reset_postdata();
-        }
     ?>
 </div>
+<?php
+        } // Fin del condicional de $related_posts
+    } // Fin del condicional de $post_categories
+?>
