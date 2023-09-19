f80ce37c2a7d0e63d06d7d951270a2dbd993557f
Chano Vera <contacto@chanovera.com>
2023-09-19 12:43:51 -0600

    ajuste al contenido de los datos del post sidebar

--- parts/sidebars/blog.php
+++ parts/sidebars/blog.php
@@ -3,5 +3,6 @@
         include (TEMPLATEPATH. '/parts/widgets/tags-cloud.php');
         include (TEMPLATEPATH. '/parts/widgets/categories.php');
         include (TEMPLATEPATH. '/parts/widgets/archive-list.php');
+        include (TEMPLATEPATH. '/parts/widgets/latest-comments.php');
     ?>
 </aside>
