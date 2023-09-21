f28ea4ce92b241f4c7f335a8032f581f02b03cdf
Chano Vera <contacto@chanovera.com>
2023-09-21 13:24:54 -0600

    ajuste a la visualización del contenido del artículo - diseño de como se muestra el código

---
+++ functions/icons.php
@@ -0,0 +1,10 @@
+<?php
+
+function theme_icons() {
+    ?>
+        <style>
+            .wp-block-code code:after{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/terminal-day.svg');}
+        </style>
+    <?php
+}
+add_action('wp_head', 'theme_icons');
