2fb574d7392ac779c7c50daf5c313a1b9f61ded4
Chano <github@chanovera.com>
2023-10-31 10:13:11 -0600

    plantilla de conocer paa vivir

--- conocer.php
+++ conocer.php
@@ -1,5 +1,5 @@
 <?php /* Template name: Conocer */ get_header(); ?>
-<main id="main">
+<main id="main" class="main-content">
     <div class="container">
         <section class="section padding-section">
             <ul class="slideshow">
