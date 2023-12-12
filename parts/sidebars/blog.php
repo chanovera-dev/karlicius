d177aaee9c0760ab80254e11a2f33640ead45328
= <=>
2023-12-12 00:35:16 -0600

    blog css

--- parts/sidebars/blog.php
+++ parts/sidebars/blog.php
@@ -1,6 +1,5 @@
 <aside>
     <?php
-        include (TEMPLATEPATH. '/parts/widgets/tags-cloud.php');
         include (TEMPLATEPATH. '/parts/widgets/categories.php');
         include (TEMPLATEPATH. '/parts/widgets/archive-list.php');
         
