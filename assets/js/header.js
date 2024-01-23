58e4a030b1abe69132645cad3a332324857523e5
= <=>
2024-01-23 12:22:52 -0600

    ajustes

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,11 +1,7 @@
 // botón para abrir el menú mobile · vease './sections/header/menu--button.php'
 function menuMobileButton(toggleButton) {
     let menuMobile =  document.querySelector('.menu-searchform__group')
-    toggleButton.querySelector('.bars').classList.toggle('change');
 
-    if (!menuMobile.classList.contains('open')) {
-        menuMobile.classList.add('open');
-    } else {
-        menuMobile.classList.remove('open');
-    }
+    toggleButton.querySelector('.bars').classList.toggle('change');
+    menuMobile.classList.toggle('open');
 }
