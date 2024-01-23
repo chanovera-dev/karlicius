9b474d0bdc9de84b1b260687c8d1c430bb0c99a5
= <=>
2024-01-23 11:47:33 -0600

    se agrega el botón del menú principal en mobile y su js

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,3 +1,4 @@
+// botón para abrir el menú mobile · vease './sections/header/menu--button.php'
 function menuMobileButton(toggleButton) {
     toggleButton.querySelector('.bars').classList.toggle('change');
 }
