6ca82898ac6690d8adc5a611ed1b02e2f894ba65
= <=>
2024-01-23 12:14:16 -0600

    ajustes

--- sections/header/menu.php
+++ sections/header/menu.php
@@ -1,6 +1,6 @@
 <?php
     echo '
-    <div id="menu-searchform-group" class="menu-searchform-group">';
+    <div id="menu-searchform__group" class="menu-searchform__group">';
 
         $menu_primary_id = get_nav_menu_locations()['primary'];
         $items_primary = wp_get_nav_menu_items($menu_primary_id);
