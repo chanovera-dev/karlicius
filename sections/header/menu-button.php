c1462dc2987a48ce8848a70b879d02dd7b2b0adc
= <=>
2024-01-23 01:57:49 -0600

    se agrega functions.php y se carga style.css en la cabecera

--- sections/header/menu-button.php
+++ sections/header/menu-button.php
@@ -1,5 +1,7 @@
 <button class="menu-mobile__button" onclick="menuMobileButton(toggleButton)">
-    <div class="bar1"></div>
-    <div class="bar2"></div>
-    <div class="bar3"></div>
+    <div class="bars">
+        <div class="bar1"></div>
+        <div class="bar2"></div>
+        <div class="bar3"></div>
+    </div>
 </button>
