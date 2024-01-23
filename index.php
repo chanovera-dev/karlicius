feac5115012cdab02ff1198da4748a30db31731b
= <=>
2024-01-22 22:09:09 -0600

    creados 'index', 'header', 'footer' y theme.json

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+get_header();
+
+echo '
+<main id="main">
+    <div class="container">
+        <section class="section">
+            <h1>' . esc_html__('Bienvenido al index de ', 'karlicius') . bloginfo('name') . '</h1>
+        </section>
+    </div>
+<main>';
+
+get_footer();
