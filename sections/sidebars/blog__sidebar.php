8f64c9ed54898a8fec0cd19a3182ee9b7e465eeb
= <=>
2024-01-25 00:00:47 -0600

    ajustes css sidebar

--- sections/sidebars/blog__sidebar.php
+++ sections/sidebars/blog__sidebar.php
@@ -31,7 +31,7 @@ echo '
         echo '
         </ul>
     </div>
-    <div class="archive-list-widget">
+    <div class="archive-list">
         <div class="title-wrapper">
             <h2 class="title">' . esc_html__('Archivo', 'karlicius') . '</h2>
         </div>
@@ -39,5 +39,33 @@ echo '
             wp_get_archives();
         echo '
         </ul>
+    </div>
+    <div class="latest-comments">';
+
+    if ($comments = $wpdb->get_results(
+        "SELECT comment_author, comment_author_url,
+        comment_ID, comment_post_ID, comment_content
+        FROM $wpdb->comments
+        WHERE comment_approved = '1'
+        ORDER BY comment_date_gmt DESC LIMIT 6")) :
+        echo '
+        <div class="title-wrapper">
+            <h2 class="title">' . esc_html__('Comentarios más recientes', 'karlicius') . '</h2>
+        </div>
+        <ul class="latest-comments__list">';
+            global $comment;
+            foreach ($comments as $comment) {
+                echo '<li>';
+                echo get_avatar( get_the_author_meta('email'), '43' );
+                echo '<div class="comment"><p><b>' . get_comment_author_link($comment->comment_ID) . '</b>';
+                echo '<a href="' . get_permalink($comment->comment_post_ID) . '#comment-' . $comment->comment_ID . '">' . get_the_title($comment->comment_post_ID) . '</a></p>';
+                echo '<p>' . $comment->comment_content . '</p>';
+                echo '</div>';
+                echo '</li>';
+            }
+        echo '
+        </ul>';
+    endif;
+    echo '
     </div>
 </aside>';
