af651939abc739809ba5a6f804594bbcc8d36e05
= <=>
2024-01-24 23:17:57 -0600

    ajustes css header

--- sections/sidebars/blog__sidebar.php
+++ sections/sidebars/blog__sidebar.php
@@ -1,3 +1,13 @@
+<?php
+echo '
 <aside>
-    
-</aside>
+    <div class="archive-list-widget">
+        <div class="title-wrapper">
+            <h3 class="title">' . esc_html__('Archivos', 'karlicius') . '</h3>
+        </div>
+        <ul>';
+            wp_get_archives();
+        echo '
+        </ul>
+    </div>
+</aside>';
