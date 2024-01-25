50a2563a6f1bd652a65c8173aad95e28d3686c8a
= <=>
2024-01-24 23:26:10 -0600

    ajustes css sidebar

--- functions/templates.php
+++ functions/templates.php
@@ -5,6 +5,7 @@ function karlicius_posts_styles() {
     if ( is_home() or is_page_template('home.php') or is_archive() or is_search() ) {
         wp_enqueue_style( 'posts-styles', get_template_directory_uri() . '/assets/css/posts.css' );
         wp_enqueue_style( 'pagination-styles', get_template_directory_uri() . '/assets/css/pagination.css' );
+        wp_enqueue_style( 'sidebar-styles', get_template_directory_uri() . '/assets/css/sidebar.css' );
     }
 }
 add_action( 'wp_enqueue_scripts', 'karlicius_posts_styles' );
