f912f49f6e2e999a2c6514f4a42f2743b918fbfe
= <=>
2024-01-24 22:58:17 -0600

    ajustes css header

--- functions/breakpoints.php
+++ functions/breakpoints.php
@@ -15,7 +15,7 @@ function karlicius_theme_custom_breakpoints() {
                 .menu-mobile__button{position:relative;padding:0;border:0;background-color:transparent;width:18px;height:14px;z-index:4;}
                 .menu-mobile__button:hover{cursor:pointer;}
                 .menu-mobile__button .bars{position:relative;display:inline-block;width:18px;height:14px;transition:all .3s ease;}
-                .menu-mobile__button .bars :is(.bar1,.bar2,.bar3){width:18px;height:1px;transition:.4s;background-color:var(--wp--preset--color--attenuated);}
+                .menu-mobile__button .bars :is(.bar1,.bar2,.bar3){width:18px;height:1px;transition:.4s;background-color:var(--wp--preset--color--attenuated);transition:all .3s ease;}
                 .menu-mobile__button .bars:hover :is(.bar1,.bar2,.bar3){background-color:var(--wp--preset--color--text);}
                 .menu-mobile__button .bars > div:not(:last-child){margin-bottom:5px;}
                 /* botón para abrir el menú mobile · activo */
