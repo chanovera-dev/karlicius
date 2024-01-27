e7753211d665ffa0b71e005f478588945c417f06
= <=>
2024-01-27 14:31:28 -0600

    footer

--- footer.php
+++ footer.php
@@ -1,7 +1,10 @@
 <?php
         echo '
         <footer id="main-footer" class="container">
-            <section class="section"></section>
+            <section class="section">
+
+                <div class="copyright"><p>© '. get_bloginfo( 'name' ) . ' · ' . date("Y") . '</p></div>
+            </section>
         </footer>';
         wp_footer();
     echo '
