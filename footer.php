c411e605b7bb1cd007967ab907d80b52b59cbedc
= <=>
2024-01-27 14:33:25 -0600

    footer

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
         <footer id="main-footer" class="container">
             <section class="section">
 
-                <div class="copyright"><p>© '. get_bloginfo( 'name' ) . ' · ' . date("Y") . '</p></div>
+                <div class="copyright"><p>© '. get_bloginfo( 'name' ) . ' ' . date("Y") . ' · ' . esc_html__('Todos los derechos reservados', 'karlicius') . '</p></div>
             </section>
         </footer>';
         wp_footer();
