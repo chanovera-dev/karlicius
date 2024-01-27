21bb019cc212f9ded7506d36befa29080ef0b7e1
= <=>
2024-01-27 01:22:43 -0600

    cabecera del artículo

--- assets/js/header.js
+++ assets/js/header.js
@@ -5,3 +5,30 @@ function menuMobileButton(toggleButton) {
     toggleButton.querySelector('.bars').classList.toggle('change');
     menuMobile.classList.toggle('open');
 }
+
+function scrollPage() {
+    const body = document.body;
+    const scrollUp = "scroll-up";
+    const scrollDown = "scroll-down";
+    let lastScroll = 0;
+    
+    window.addEventListener("scroll", () => {
+      const currentScroll = window.pageYOffset;
+      if (currentScroll <= 0) {
+        body.classList.remove(scrollUp);
+        return;
+      }
+      
+      if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+        // down
+        body.classList.remove(scrollUp);
+        body.classList.add(scrollDown);
+      } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+        // up
+        body.classList.remove(scrollDown);
+        body.classList.add(scrollUp);
+      }
+      lastScroll = currentScroll;
+    });
+  }
+  scrollPage();
