ba38121757a93c1aa7d506b58c3273507919cfdb
= <=>
2024-01-27 01:05:18 -0600

    cabecera del artículo

--- functions/backgrounds.php
+++ functions/backgrounds.php
@@ -11,8 +11,14 @@ function karlicius_theme_custom_backgrounds() {
                 .single-post #main .header-post.container .background-blur{position:absolute;top:0;left:0;width:100%;height:100%;z-index:0;}
                 .single-post #main .header-post.container .section{position:relative;z-index:1;color:#fff;}
                 .single-post #main .header-post.container .section aside .share a:where(:not(.wp-element-button)):any-link{color:#fff;}
+
                 @supports ( (-webkit-backdrop-filter:saturate(180%) blur(20px)) or (backdrop-filter:saturate(180%) blur(20px)) ){
                 .background-blur{background:rgba(0,0,0,.4); backdrop-filter:saturate(180%) blur(20px); -webkit-backdrop-filter:saturate(180%) blur(20px);}
+
+                @media(min-width:1024px){
+                    .single-post #main-header .menu-searchform__group nav ul li a{color:#fff;opacity:.5;}
+                    .single-post #main-header .menu-searchform__group nav ul li a:hover{opacity:1;}
+                }
             }
             <?php endif; ?>
 
