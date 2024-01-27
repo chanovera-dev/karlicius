8908220e9092774ea8b84fde3a491e0ff20e6b19
= <=>
2024-01-27 00:47:04 -0600

    cabecera del artículo

--- functions/backgrounds.php
+++ functions/backgrounds.php
@@ -6,6 +6,7 @@ function karlicius_theme_custom_backgrounds() {
             <?php if ( has_post_thumbnail() == false ) : ?>
             
             <?php else: ?>
+                .single-post #main-header{position:fixed;}
                 .single-post #main .header-post.container{background:linear-gradient(to bottom, #0000005b, #00000000), url('<?php the_post_thumbnail_url( 'full' ); ?>'); background-repeat:no-repeat; background-position:50% 50%; background-size:cover;}
             <?php endif; ?>
 
