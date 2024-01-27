ba1437cf760b9e4488abdc901cd2a487b90cd20b
= <=>
2024-01-27 00:52:21 -0600

    cabecera del artículo

--- functions/backgrounds.php
+++ functions/backgrounds.php
@@ -7,7 +7,7 @@ function karlicius_theme_custom_backgrounds() {
             
             <?php else: ?>
                 .single-post #main-header{position:fixed;width:100%;background-color:transparent;}
-                .single-post #main .header-post.container{padding-top:var(--height--header);background:linear-gradient(to bottom, #0000005b, #00000000), url('<?php the_post_thumbnail_url( 'full' ); ?>'); background-repeat:no-repeat; background-position:50% 50%; background-size:cover;filter:blur(20px);}
+                .single-post #main .header-post.container{padding-top:var(--height--header);background:linear-gradient(to bottom, #0000005b, #00000000), url('<?php the_post_thumbnail_url( 'full' ); ?>'); background-repeat:no-repeat; background-position:50% 50%; background-size:cover;}
             <?php endif; ?>
 
         </style>
