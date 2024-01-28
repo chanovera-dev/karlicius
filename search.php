38d1e1548254ad6fabe2ab1aa2239f411f9f3a7e
= <=>
2024-01-28 11:46:28 -0600

    search

--- search.php
+++ search.php
@@ -6,7 +6,7 @@ echo '
     <div class="container">
         <section class="section">
 
-            <h1 class="title-page">'; echo the_search_query(); echo '</h1>';
+            <h1 class="title-page">'; echo esc_thml__('Búsqueda encontrada sobre ', 'karlicius'); echo the_search_query(); echo '</h1>';
         
             if ( have_posts() ) {
 
