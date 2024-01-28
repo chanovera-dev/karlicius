4e329cdcb54841d1cd800dbdca21a75af0fcbfd3
= <=>
2024-01-28 10:48:21 -0600

    footer

---
+++ archive.php
@@ -0,0 +1,47 @@
+<?php 
+get_header();
+
+echo '
+<main id="main">
+    <div class="container">
+        <section class="section>';
+            the_archive_title( '<h1>', '</h1>' );
+        echo '
+        </section>"
+        <section class="section">';
+        
+            if ( have_posts() ) {
+
+                echo '
+                <div class="posts__wrapper">
+
+                    <div class="posts">';
+                    
+                    while( have_posts() ) {
+
+                        the_post();
+                        get_template_part( 'templates/content', 'archive' );
+
+                    }
+                    
+                    echo '
+                    </div>';
+                    the_posts_pagination();
+
+                echo '
+                </div>';
+
+                include(TEMPLATEPATH . '/sections/sidebars/blog__sidebar.php');
+
+            } else {
+
+                echo '<p>' . esc_html__('No se encontraron artículos', 'karlicius') . '</p>';
+                
+            }
+
+        echo '
+        </section>
+    </div>
+</main>';
+
+get_footer();
