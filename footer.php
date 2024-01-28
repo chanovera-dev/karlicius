0a0d818d72e99073ccb4e5b15eb39f09dade4c5d
= <=>
2024-01-27 18:33:32 -0600

    footer

--- footer.php
+++ footer.php
@@ -2,7 +2,23 @@
         echo '
         <footer id="main-footer" class="container">
             <section class="section">
-
+                <aside>';
+                    $menu_id = get_nav_menu_locations()[ 'secondary' ];
+                    $menu = wp_get_nav_menu_object( $menu_id );
+                    $items = wp_get_nav_menu_items( $menu_id );
+                    if (!empty($items)) {
+                        echo '
+                        <h2>' . $menu->name . '</h2>';
+                        wp_nav_menu(
+                            array(
+                                'container' => 'nav', 
+                                'container_class' => 'secondary', 
+                                'theme_location' => 'secondary',
+                            ) 
+                        ); 
+                    }
+                echo '
+                </aside>
                 <div class="copyright"><p>© '. get_bloginfo( 'name' ) . ' ' . date("Y") . ' · ' . esc_html__('Todos los derechos reservados', 'karlicius') . '</p></div>
             </section>
         </footer>';
