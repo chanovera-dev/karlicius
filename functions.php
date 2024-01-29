71feaae745436cdd327975a20a4e838830bfeea0
= <=>
2024-01-28 22:59:57 -0600

    condicional para cargar un estilo css si woocommerce está activo

--- functions.php
+++ functions.php
@@ -116,3 +116,16 @@ add_filter('get_archives_link', 'custom_archives_link', 10, 6);
 // deshabilita contact form 7 en todas las páginas | revisar components.php para ver que páginas tienen habilitado contact form 7
 add_filter( 'wpcf7_load_js', '__return_false' );
 add_filter( 'wpcf7_load_css', '__return_false' );
+
+
+
+function desactivar_enlighter_except_single() {
+    // Verifica si NO estás en una entrada única (single.php)
+    if (!is_single()) {
+        // Desactiva el plugin Enlighter
+        deactivate_plugins('enlighter/enlighter.php');
+    }
+}
+
+// Engancha la función al gancho 'init'
+add_action('init', 'desactivar_enlighter_except_single');
