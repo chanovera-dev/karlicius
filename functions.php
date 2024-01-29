3c6c434f7dc8602190bf63c692b74e8efcdd72ab
= <=>
2024-01-28 23:45:01 -0600

    ajustes

--- functions.php
+++ functions.php
@@ -124,21 +124,25 @@ function remove_custom_styles(){
         wp_dequeue_style("enlighterjs");
         wp_deregister_style("enlighterjs");
     }
+    wp_dequeue_style("wp-emoji-styles");
+    wp_deregister_style("wp-emoji-styles");
+    wp_dequeue_style("core-block-supports-duotone");
+    wp_deregister_style("core-block-supports-duotone");
 }
 add_action("wp_print_styles", "remove_custom_styles");
 
 
 
-function ver_archivos_cargados(){
-    global $wp_styles;
-    global $wp_scripts;
-    echo 'STYLES:';
-    echo '<pre>';
-    var_dump($wp_styles->queue);
-    echo '</pre>';
-    echo 'SCRIPTS:';
-    echo '<pre>';
-    var_dump($wp_scripts->queue);
-    echo '</pre>';
-}
-add_action("wp_footer", "ver_archivos_cargados");
+// function ver_archivos_cargados(){
+//     global $wp_styles;
+//     global $wp_scripts;
+//     echo 'STYLES:';
+//     echo '<pre>';
+//     var_dump($wp_styles->queue);
+//     echo '</pre>';
+//     echo 'SCRIPTS:';
+//     echo '<pre>';
+//     var_dump($wp_scripts->queue);
+//     echo '</pre>';
+// }
+// add_action("wp_footer", "ver_archivos_cargados");
