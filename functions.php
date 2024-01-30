4c9658ac4260718b6c625cc324857e797c596b28
= <=>
2024-01-30 08:52:45 -0600

    ajustes

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 // carga componentes (estilos, javascript, etc) en el header
 function load_components_header() {
     // Estilos globales
-    wp_dequeue_style( 'wp-block-library' );
+    //wp_dequeue_style( 'wp-block-library' );
     wp_register_style( 'global', get_template_directory_uri() . '/style.css', '', 1, 'all' );
     wp_enqueue_style( 'global' );
 }
