dc3ecdd3728c8f4adb64ca6ea820cb4e352dd869
Chano <github@chanovera.com>
2024-02-07 10:56:23 -0600

    agregar clase al header si el menú mobile está cerrado

--- assets/js/header.js
+++ assets/js/header.js
@@ -10,6 +10,8 @@ function scrollPage() {
     const body = document.body;
     const scrollUp = "scroll-up";
     const scrollDown = "scroll-down";
+    const header = document.header;
+    const menuMobile = document.getElementById('menu-mobile-searchform__group');
     let lastScroll = 0;
     
     window.addEventListener("scroll", () => {
@@ -23,10 +25,16 @@ function scrollPage() {
         // down
         body.classList.remove(scrollUp);
         body.classList.add(scrollDown);
+        if (!menuMobile.classList.contains('open')) {
+          header.classList.add('shadow');
+        }
       } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
         // up
         body.classList.remove(scrollDown);
         body.classList.add(scrollUp);
+        if (!menuMobile.classList.contains('open')) {
+          header.classList.add('shadow');
+        }
       }
       lastScroll = currentScroll;
     });
