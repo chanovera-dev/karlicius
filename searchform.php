d9dacfa9d64bff208581be414899a64647f8c490
Chano <github@chanovera.com>
2024-02-07 12:45:31 -0600

    edición de searchform · probando el botón de enviar

--- searchform.php
+++ searchform.php
@@ -1,4 +1,5 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
     <input type="search" class="search-field" placeholder="<?php esc_html__('Buscar', 'karlicius') ?>" value="" name="s">
-    <input type="submit" class="search-submit" value="Buscar">
+
+    <button type="submit" class="search-submit">Buscar</button>
 </form>
