69abed1e3c6621c1f860557dfbb0825dde463386
Chano <github@chanovera.com>
2024-02-08 14:30:11 -0600

    mostrar el carrito en tiempo real.

--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -14,3 +14,24 @@ add_theme_support( 'wc-product-gallery-slider' );
 
 //Disable all woocommerce stylesheets
 add_filter( 'woocommerce_enqueue_styles', '__return_false' );
+
+
+
+// mostrar el carrito en tiempo real
+add_filter( 'woocommerce_add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment' );
+
+function woocommerce_header_add_to_cart_fragment( $fragments ) {
+	global $woocommerce;
+
+	ob_start();
+
+    echo '
+	<a class="counter cart-customlocation" href="'; echo esc_url(wc_get_cart_url()); echo '">
+        <svg width="20px" height="20px">
+            <use xlink:href="'.get_template_directory_uri().'/assets/img/sprite.svg#cart-20"></use>
+        </svg>
+        <div class="wrapper"><span class="number">'; echo sprintf ( WC()->cart->get_cart_contents_count() );  echo'</span></div>
+    </a>';
+	$fragments['a.cart-customlocation'] = ob_get_clean();
+	return $fragments;
+}
