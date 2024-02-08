fe0ccc61fa855565801502072c5aaf5b38dea96a
Chano <github@chanovera.com>
2024-02-08 02:02:22 -0600

    searchform desktop · ajustando el js

--- functions/breakpoints.php
+++ functions/breakpoints.php
@@ -41,14 +41,15 @@ function karlicius_theme_custom_breakpoints() {
                     echo '#main-header.container .section{display:grid;grid-template-columns:1fr auto 1fr;}';
                 } else {}
                 ?>
-                /* formulario de búsqueda */
-                .search-form{max-width:38px;}
-                .search-form input[type=search]{opacity:0;}
-                #menu-searchform__group .search-form .search-submit__button{width:38px;height:38px;top:0;right:0;border:1px solid transparent;background-color:transparent;color:var(--wp--preset--color--text-b);z-index:1;}
-                #menu-searchform__group .search-form:hover .search-submit__button{color:var(--wp--preset--color--text);}
-                #menu-searchform__group #close-search__button{display:grid;place-content:center;position:absolute;top:0;right:0;width:38px;height:38px;border:1px solid transparent;background-color:transparent;color:var(--wp--preset--color--text-b);transition:all .3s ease;z-index:2;}
-                #menu-searchform__group #close-search__button:hover{color:var(--wp--preset--color--text);}
-
+                /* formulario de búsqueda · cerrado */
+                #menu-searchform__group .search-form__wrapper .search-form{max-width:38px;}
+                #menu-searchform__group .search-form__wrapper .search-form input[type=search]{opacity:0;}
+                #menu-searchform__group .search-form__wrapper .search-form .search-submit__button{width:38px;height:38px;top:0;right:0;border:1px solid transparent;background-color:transparent;color:var(--wp--preset--color--text-b);z-index:1;}
+                #menu-searchform__group .search-form__wrapper .search-form:hover .search-submit__button{color:var(--wp--preset--color--text);}
+                #menu-searchform__group .search-form__wrapper #close-search__button{display:grid;place-content:center;position:absolute;top:0;right:0;width:38px;height:38px;border:1px solid transparent;background-color:transparent;color:var(--wp--preset--color--text-b);transition:all .3s ease;z-index:2;}
+                #menu-searchform__group .search-form__wrapper #close-search__button:hover{color:var(--wp--preset--color--text);}
+                /* formulario de búsqueda · abierto */
+                #menu-searchform__group .search-form__wrapper.open .search-form{max-width:300px;}
 
 
                 /* M E N Ú   M O B I L E */
