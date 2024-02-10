502b990d59aea39aae352138d96b764060a2c78b
Chano <github@chanovera.com>
2024-02-10 17:18:03 -0600

    quitando el soporte para wc blocks style

--- functions.php
+++ functions.php
@@ -18,6 +18,7 @@ add_action( 'wp_enqueue_scripts', 'load_components_header' );
 function load_components_footer(){
     // JS de efectos en la cabecera
     wp_enqueue_script( 'header-scripts', get_template_directory_uri() . '/assets/js/header.js', array(), '1.0', true );
+    wp_dequeue_style( 'wc-blocks-style' );
 }
 add_action( 'get_footer', 'load_components_footer' );
 
