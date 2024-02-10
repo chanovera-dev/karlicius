06b2daec9a7176ded313644746167462906179b8
Chano <github@chanovera.com>
2024-02-10 16:44:49 -0600

    ajustes a la accesibilidad del cuadro de búsqueda

--- searchform.php
+++ searchform.php
@@ -1,7 +1,5 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-    <label for="search-field" class="screen-reader-text"><?php esc_html__('Buscar', 'karlicius'); ?></label>
-    <input type="search" id="search-field" class="search-field" placeholder="<?php esc_html__('Buscar', 'karlicius') ?>" value="" name="s">
-    
+    <input type="search" id="search-field" class="search-field" placeholder="<?php esc_html__('Buscar', 'karlicius') ?>" value="" name="s" aria-label="search">
     <button type="submit" class="search-submit__button" aria-label="Botón buscar">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
