5eb4493816f8c28ab48f12dbcea7ccd1e13efa9d
Chano <github@chanovera.com>
2024-02-10 16:24:42 -0600

    ajustes al cuadro de búsqueda en modo escritorio

--- functions/breakpoints.php
+++ functions/breakpoints.php
@@ -94,11 +94,11 @@ function karlicius_theme_custom_breakpoints() {
                 #menu-searchform__group .search-form__wrapper .search-form .search-submit__button{width:38px;height:38px;top:0;right:0;border:1px solid transparent;background-color:transparent;color:var(--wp--preset--color--text-b);z-index:1;}
                 #menu-searchform__group .search-form__wrapper .search-form .search-submit__button:focus{border:1px solid var(--wp--preset--color--button-focus);background-color:var(--wp--preset--color--button-focus);color:#fff;}
                 #menu-searchform__group .search-form__wrapper .search-form:hover .search-submit__button{color:var(--wp--preset--color--text);}
-                #menu-searchform__group .search-form__wrapper #close-search__button{display:grid;place-content:center;position:absolute;top:0;right:0;width:38px;height:38px;border:1px solid transparent;background-color:transparent;color:var(--wp--preset--color--text-b);opacity:0;transition:all .3s ease;z-index:2;}
+                #menu-searchform__group .search-form__wrapper #close-search__button{display:grid;place-content:center;position:absolute;top:0;left:0;width:38px;height:38px;border:1px solid transparent;background-color:transparent;color:var(--wp--preset--color--text-b);opacity:0;transition:all .3s ease;z-index:2;}
                 #menu-searchform__group .search-form__wrapper #close-search__button:hover{color:var(--wp--preset--color--text);}
                 /* formulario de búsqueda · abierto */
-                #menu-searchform__group .search-form__wrapper.open .search-form{width:500px;max-width:500px;padding-right:38px;}
-                #menu-searchform__group .search-form__wrapper.open .search-form input[type=search]{opacity:1;padding-right:38px;}
+                #menu-searchform__group .search-form__wrapper.open .search-form{width:500px;max-width:500px;}
+                #menu-searchform__group .search-form__wrapper.open .search-form input[type=search]{opacity:1;padding-left:48px;padding-right:38px;}
                 #menu-searchform__group .search-form__wrapper.open .search-form .search-submit__button{width:28px;height:28px;right:43px;top:5px;border:1px solid var(--wp--preset--color--button);background-color:var(--wp--preset--color--button);color:#fff;border-radius:3px;}
                 #menu-searchform__group .search-form__wrapper.open .search-form .search-submit__button:hover{border:1px solid var(--wp--preset--color--button-hover);background-color:var(--wp--preset--color--button-hover);}
                 #menu-searchform__group .search-form__wrapper.open .search-form .search-submit__button:focus{border:1px solid var(--wp-preset--color--button-focus);background-color:var(--wp--preset--color--button-focus);}
