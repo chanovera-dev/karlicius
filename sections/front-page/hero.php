d764295964233fcf7f88e09336fe4fb5e239de3f
Chano <github@chanovera.com>
2024-02-10 14:57:10 -0600

    contenido de hero

--- sections/front-page/hero.php
+++ sections/front-page/hero.php
@@ -1,6 +1,13 @@
 <div id="hero" class="container">
     <section class="section">
-        <div class="intro">testing</div>
-        <div class="hero-picture"></div>
+        <div class="intro">
+            <p>Hola, soy Chano Vera</p>
+            <h1>Desarrollo <span>sitios web basados en Wordpress</span>. <span>Tiendas</span>. <span>Blogs</span>. <span>Servicios</span>.</h1>
+            <p>Me dedico a desarrollar sitios webs totalmente a la medida, siempre mejorando y entregando mejores productos.</p>
+            <button id="agendar" onclick="agendar()">Hablemos</button>
+        </div>
+        <div class="hero-picture">
+            <img src="<?php echo get_theme_mod('img_hero', get_bloginfo('template_url') . '/assets/img/webDesign.webp'); ?>" alt="computadoras mostrando sitios web">
+        </div>
     </section>
 </div>
