41488c54f02e674316e965f5751db7ba535790fd
Chano <github@chanovera.com>
2024-02-10 16:57:13 -0600

    quitando el soporte de duotone del sitio

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@
 function load_components_header() {
     // Estilos globales
     wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style( 'core-block-supports-duotone' );
     //wp_register_style( 'global', get_template_directory_uri() . '/style.css', '', 1, 'all' );
     //wp_enqueue_style( 'global' );
 }
