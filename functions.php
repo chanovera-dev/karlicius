064757e427b92d4f4ef73118128a4998d8379c30
Chano <github@chanovera.com>
2024-02-10 18:31:56 -0600

    mover jQuery al footer

--- functions.php
+++ functions.php
@@ -4,8 +4,8 @@
 function load_components_header() {
     // Estilos globales
     wp_dequeue_style( 'wp-block-library' );
-    wp_deregister_script('wp-polyfill');
-    wp_deregister_script('regenerator-runtime');
+    // wp_deregister_script('wp-polyfill');
+    // wp_deregister_script('regenerator-runtime');
     //wp_register_style( 'global', get_template_directory_uri() . '/style.css', '', 1, 'all' );
     //wp_enqueue_style( 'global' );
 }
