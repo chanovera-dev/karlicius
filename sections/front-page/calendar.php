ed848f13a2504e3613c158879570b9e1359d3d50
Cristian <github@chanovera.com>
2024-02-12 17:57:14 -0600

    agregada la sección de calendario de contacto al frontpage

--- sections/front-page/calendar.php
+++ sections/front-page/calendar.php
@@ -1,5 +1,5 @@
 <div class="container">
     <section class="section">
-        <?php echo do_shortcode('[Cal url=/chanovera/30min]'); ?>
+        <?php do_shortcode('[Cal url=/chanovera/30min]'); ?>
     </section>
 </div>
