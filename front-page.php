ff0f9a80cd615900f4f5d048365486dfc26a73b0
Cristian <github@chanovera.com>
2024-02-12 16:14:38 -0600

    agregada la sección de calendario de contacto al frontpage

--- front-page.php
+++ front-page.php
@@ -4,6 +4,7 @@ get_header();
 echo '
 <main id="main">';
     include(TEMPLATEPATH . '/sections/front-page/hero.php');
+    include(TEMPLATEPATH . '/sections/front-page/calendar.php');
 echo '
 </main>';
 
