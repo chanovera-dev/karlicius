d2c76464b0db880dbe0295b7bacf80702b2fbdad
Cristian <github@chanovera.com>
2024-02-13 22:49:43 -0600

    pruebas para fondo en header en page.php

--- functions.php
+++ functions.php
@@ -58,7 +58,7 @@ function karlicius_theme_support(){
     
     add_theme_support( 'customize-selective-refresh-widgets' );
 
-    add_theme_support( 'post-thumbnails', array( 'post' ) ); // Add it for posts
+    add_theme_support( 'post-thumbnails', array( 'post', 'page' ) ); // Add it for posts
 	set_post_thumbnail_size( 200, 200, true ); // Normal post thumbnails, hard crop mode
 	add_image_size( 'post-image', 600, 200, true ); // Post thumbnails, hard crop mode
 	add_image_size( 'slider-image', 920, 300, true ); // Post thumbnails for slider, hard crop mode
