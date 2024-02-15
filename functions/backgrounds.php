a8ff3a1e65d8a2bfa92ad52918dbb4749522c2b5
Cristian <github@chanovera.com>
2024-02-14 18:00:49 -0600

    pruebas para fondo en header en page.php

--- functions/backgrounds.php
+++ functions/backgrounds.php
@@ -46,9 +46,18 @@ function karlicius_theme_custom_backgrounds() {
                 }
 
                 @media(min-width:1024px){
+                    /* menú */
                     :is(.single-post,.page-template-default) .menu-searchform__group nav ul li a{color:#ffffff86;}
                     :is(.single-post,.page-template-default) .menu-searchform__group nav ul li a:hover{color:#fff;}
                     :is(.single-post,.page-template-default) .menu-searchform__group nav ul li a:focus{color:#fff;}
+                    /* formulario de búsqueda */
+                    :is(.single-post,.page-template-default) #menu-searchform__group .search-form__wrapper #close-search__button{color:#ffffff86;}
+                    :is(.single-post,.page-template-default) #menu-searchform__group .search-form__wrapper #close-search__button:hover{color:#fff;}
+                    :is(.single-post,.page-template-default) #menu-searchform__group .search-form__wrapper #close-search__button:focus{color:#fff;}
+                        /* abierto */
+                        :is(.single-post,.page-template-default) #menu-searchform__group .search-form__wrapper.open #close-search__button{color:var(--wp--preset--color--text-b);}
+                        :is(.single-post,.page-template-default) #menu-searchform__group .search-form__wrapper.open #close-search__button:hover{color:var(--wp--preset--color--text);}
+                        
                 }
             <?php endif; ?>
 
