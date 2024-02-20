4a62c1b1a9eefe50f27ff58b69ba22ec94420e8a
Cristian <github@chanovera.com>
2024-02-20 13:21:23 -0600

    reorganizando el hero

--- sections/front-page/hero.php
+++ sections/front-page/hero.php
@@ -10,9 +10,9 @@
             </div>
         </div>
         <?php if ( has_post_thumbnail() == false ) : ?>
-        <div class="hero-picture">
+        <!-- <div class="hero-picture">
             <img src="<?php echo get_theme_mod('img_hero', get_bloginfo('template_url') . '/assets/img/webDesign.webp'); ?>" alt="computadoras mostrando sitios web">
-        </div>
+        </div> -->
         <?php else: ?>
             
         <?php endif; ?>
